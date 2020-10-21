c60f83eaad77289d29fbc716923855a321d56b6e
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-10-21 18:39:25 +0700

    Update main.c

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -3,35 +3,60 @@
 
 int main()
 {
-	long long int a, b,indx=0,tochka=0,treq=0,numeral,part1=0,part2=0,step=1,part1_size=0,p1=0;
+	long long int a, b,indx=0,tochka=0,treq=0,part1=0,part2=0,step=1,part1_size=0,p1=0;
 	char X[13];
 	if (scanf("%lld", &a) != 1) 
 	{
-	    exit(0);
+	    return 0;
 	}
 	if (scanf("%lld", &b) != 1)
 	{
-		exit(0);
+		return 0;
 	}
 	if (scanf("%s", X) != 1)
 	{
-		exit(0);
+		return 0;
 	}
 	if(a<2||a>16||b<2||b>16)
 	{
 	    printf("%s", "bad input");
-		exit(0);
+		return 0;
 	}
-	for (; indx < 13 &&X[indx]!=0; indx++)
+	for (indx; indx < 13 &&X[indx]!=0; indx++)
 	{
-	    numeral=-1;
-		(X[indx] - '.' == 0) ? (tochka = 1, numeral=0) : ((X[indx] - '0' >= 0 && X[indx] - '9' <= 0) ? numeral = X[indx] - '0' : ((X[indx] - 'A' >= 0 && X[indx] - 'F' <= 0) ? numeral = X[indx] - 'A' + 10 : ((X[indx] - 'a' >= 0 && X[indx] - 'f' <= 0) ? numeral = X[indx] - 'a' + 10 : (-1))));
+	    int numeral=-1;
+		if(X[indx]-'.'==0)
+		{
+			tochka=1;
+			numeral=0;
+		}else
+		{
+			if(X[indx]-'0'>=0&&X[indx]-'0'<=9)
+			{
+				numeral = X[indx] - '0';
+			}else
+			{
+				if(X[indx] - 'A' >= 0 && X[indx] - 'F' <= 0)
+				{
+					numeral = X[indx] - 'A' + 10;
+				}else
+				{
+					if(X[indx] - 'a' >= 0 && X[indx] - 'f' <= 0)
+					{
+						numeral = X[indx] - 'a' + 10;
+					}else
+					{
+						numeral=(-1);
+					}
+				}
+			}
+		}
 		if (numeral != -1)
 		{
 		    if(numeral<0||numeral>a-1)
 		    {
 		        printf("%s", "bad input");
-			    exit(0);
+			    return 0;
 		    }
 		    if(tochka==0)
 		    {
@@ -70,15 +95,20 @@ int main()
 		else
 		{
 			printf("%s", "bad input");
-			exit(0);
+			return 0;
 		}
 	}
 	if(p1>1)
 	{
 	    printf("%s", "bad input");
-		exit(0);
+		return 0;
 	}
 	long long int copy_part1=part1,copy_part2=part2;
+	if(part1==0&&part2==0&&tochka==2)
+	{
+		printf("%s","bad input");
+		return 0;
+	}
 	while (copy_part1 != 0)
 	{
 		copy_part1 /= b;
@@ -95,32 +125,34 @@ int main()
 		symbol = part1 % b;
 		if (symbol <= 9)
 		{
-			symb = '0' + symbol;
+			symb = (char)((int)'0' + (int)symbol);
 		}
 		else
 		{
-			symb = 'A' + symbol - 10;
+			symb = (char)((int)'a' + (int)symbol - 10);
 		}
 		part1_end[i] = symb;
 	}
+	int cv=0;
 	for(long long int i=0;i<15;i++)
 	{
-	    symbol =(part2*b)/step;
+			symbol =(part2*b)/step;
 	    if (symbol <= 9)
 		{
-			symb = '0' + symbol;
+			symb = (char)((int)'0' + (int)symbol);
 		}
 		else
 		{
-			symb = 'A' + symbol - 10;
+			symb = (char)((int)'a' + (int)symbol - 10);
 		}
 		part2_end[i] = symb;
 		part2=part2*b-symbol*step;
 	}
+	/*
 	long long int r=0;
 	for(long long int i=part1_size-1;i>-1;i--)
 	{
-	    if(part1_end[i]!=0)
+	    if(part1_end[i]!='0')
 	    {
 	        r=1;
 	    }
@@ -132,7 +164,7 @@ int main()
 	long long int y=0;
 	for(long long int i=14;i>-1;i--)
 	{
-	    if(part2_end[i]!='0')
+	    if(part2_end[i]!='-')
 	    {
 	        y=i+1;
 	        break;
@@ -155,6 +187,22 @@ int main()
 	        {
 	            printf("%c",part2_end[i]);
 	        }
+	}
+	*/
+	for(long long int i=part1_size-1;i>-1;i--)
+	{
+	    printf("%c",part1_end[i]);
+	}
+	if(tochka==2&&copy_part2!=0)
+	{
+		printf("%c",'.');
+		for(long long int i=0;i<12;i++)
+		{
+			if(part2_end[i]!='-')
+			{
+				printf("%c",part2_end[i]);
+			}
+		}
 	}
 	return 0;
 }
