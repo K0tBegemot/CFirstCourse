92c53bb099e087bdeadc7a5eb84bb0e78d357a73
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-11-05 19:13:51 +0700

    Update main.c

--- lab3-0/src/main.c
+++ lab3-0/src/main.c
@@ -2,56 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-void quicksort(int *mass,int first,int end)
+void quicksort(long long int *mass,long long int first,long long int end)
 {
+	
 	if(end-first==0)//end-first==1||
 	{
 		
 	}else
 	{
-		int i=first,ii=end-1;
-		while(i<ii)
+	
+		long long pivot=mass[(first+end)/2];
+		long long int i=first,ii=end;
+		while(i<=ii)
 		{
-			if(mass[i]<=mass[end])
+			while(mass[i]<pivot)
 			{
 				i+=1;
-				continue;
 			}
-			if(mass[ii]>=mass[end])
+			while(mass[ii]>pivot)
 			{
 				ii-=1;
-				continue;
 			}
-			int t;
+			if(i<=ii)
+			{
+			long long int t;
 			t=mass[i];
 			mass[i]=mass[ii];
 			mass[ii]=t;
 			i+=1;
 			ii-=1;
+			}
 		}
-		if(mass[ii]>mass[end])
-		{
-			int t;
-			t=mass[ii];
-			mass[ii]=mass[end];
-			mass[end]=t;
-		}
-		if(mass[i]>mass[end])
-		{
-			int t;
-			t=mass[i];
-			mass[i]=mass[end];
-			mass[end]=t;
-		}
-		/*
-		for(int iii=0;iii<15;iii++)
+        /*
+		for(int iii=0;iii<200;iii++)
 		{
 			printf("%d%c",mass[iii],' ');
 		}
 		printf("%c",'\n');
 		*/
-		quicksort(mass,first,i);
-		quicksort(mass,i+1,end);
+		if (first < ii)
+		{
+			quicksort(mass, first, ii);
+		}
+        if (i < end)
+        {
+        	quicksort(mass, i, end);
+		}
 	}
 }
 
@@ -59,21 +55,36 @@ int main()
 {
 	FILE *ptrfile=fopen("in.txt","r");
 	char delet;
-	int size;
-	if(fscanf(ptrfile,"%d%c",&size,&delet)==0)
+	long long int size;
+	if(fscanf(ptrfile,"%lld%c",&size,&delet)==0)
 	{
 		
 	}
-	int *mass=(int*)malloc(sizeof(int)*size);
+	long long int *mass=(long long int*)malloc(sizeof(long long int)*size);
+	int minimal,counter=0;
+	FILE* ptrfile2=fopen("out.txt","w");
 	for(long long int i=0;i<size;i++)
 	{
-		if(fscanf(ptrfile,"%d%c",mass+i,&delet)==0)
+		if(fscanf(ptrfile,"%lld%c",mass+i,&delet)==0)
 		{
 		    
 		}
+		if(i==0)
+		{
+			minimal=mass[i];
+		}
+		if(mass[i]>=mass[i-1]&&counter==0&&i!=0)
+		{
+			
+		}else
+		{
+			counter=1;
+		}
 	}
-	quicksort(mass,0,size-1);
-	FILE* ptrfile2=fopen("out.txt","w");
+	if(counter==1)
+	{
+		quicksort(mass,0,size-1);
+	}
 	for(long long int i=0;i<size;i++)
 	{
 		fprintf(ptrfile2,"%d%s",mass[i]," ");
