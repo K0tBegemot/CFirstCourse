cbf2a6ac524214e9c9aa977cff8bbad4671db2b2
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-11-07 16:59:22 +0700

    Update main.c

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,5 +1,67 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+void swapper(int *mass,int first,int second)
+{
+	int a=mass[first];
+	mass[first]=mass[second];
+	mass[second]=a;
+}
+
+void heap(int *mass,int *size,int index)
+{
+	int largest=index;
+	int l=2*index+1;
+	int r=2*index+2;
+	if(l<(*size)&&mass[l]>mass[index])
+	{
+		largest=l;
+	}
+	if(r<(*size)&&mass[r]>mass[index])
+	{
+		largest=r;
+	}
+	if(largest!=index)
+	{
+		swapper(mass,largest,index);
+		heap(mass,size,largest);
+	}
+}
+
+void heapsort(int *mass, int size)
+{
+	for(int i=size-1;i>-1;i--)
+	{
+		heap(mass,&size,i);
+	}
+	for(int i=size-1;i>-1;i--)
+	{
+		swapper(mass,0,i);
+		heap(mass,&i,0);
+	}
+}
+
+void printer_mass(int *mass,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		printf("%d%c",mass[i],' ');
+	}
+}
+
+int main()
+{
+	int size;
+	char deleter;
+	scanf("%d%c",&size,&deleter);
+	int *massive=(int*)malloc(sizeof(int)*size);
+	for(int i=0;i<size;i++)
+	{
+		scanf("%d%c",massive+i,&deleter);
+	}
+	heapsort(massive,size);
+	printer_mass(massive,size);
+	free(massive);
+	return 0;
 }
