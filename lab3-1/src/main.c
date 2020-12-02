cb9a8cb75a55380eff7fe41b220a89ebaada7af8
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-12-02 17:37:33 +0700

    Update main.c

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -2,59 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-void swapper(int *mass,int first,int second)
+void swap(int *mass, int first, int second)
 {
-	int a=mass[first];
-	mass[first]=mass[second];
-	mass[second]=a;
+	int a = mass[first];
+	mass[first] = mass[second];
+	mass[second] = a;
 }
 
-void heap(int *mass,int *size,int index)
+void make_heap(int *mass, int *size, int index)
 {
-	int largest=index;
-	int l=2*index+1;
-	int r=2*index+2;
-	if(l<(*size)&&mass[l]>mass[largest])
+	int largest = index;
+	int l = 2 * index + 1;
+	int r = 2 * index + 2;
+	if(l < (*size) && mass[l] > mass[largest])
 	{
-		largest=l;
+		largest = l;
 	}
-	if(r<(*size)&&mass[r]>mass[largest])
+	if(r < (*size) && mass[r] > mass[largest])
 	{
-		largest=r;
+		largest = r;
 	}
-	if(largest!=index)
+	if(largest != index)
 	{
-		swapper(mass,largest,index);
-		heap(mass,size,largest);
+		swap(mass, largest, index);
+		heap(mass, size, largest);
 	}
 }
 
-void printer_mass(int *mass,int size)
+void print_arr(int *mass, int size)
 {
-	for(int i=0;i<size;i++)
+	for(int i=0; i < size; i++)
 	{
-		printf("%d%c",mass[i],' ');
+		printf("%d%c", mass[i], ' ');
 	}
 }
 
 void heapsort(int *mass, int size)
 {
-	for(int i=size-1;i>-1;i--)
+	for(int i = size - 1; i > -1; i--)
 	{
-		heap(mass,&size,i);
+		make_heap(mass, &size, i);
 	}
-	/*
-	printer_mass(mass,size);
-	printf("%c",'\n');
-	*/
-	for(int i=size-1;i>-1;i--)
+	for(int i = size - 1; i>-1; i--)
 	{
-		swapper(mass,0,i);
-		heap(mass,&i,0);
-		/*
-		printer_mass(mass,size);
-		printf("%c",'\n');
-		*/
+		swap(mass, 0, i);
+		make_heap(mass, &i, 0);
 	}
 }
 
@@ -62,20 +54,20 @@ int main()
 {
 	int size;
 	char deleter;
-	if(scanf("%d%c",&size,&deleter)==0)
+	if(scanf("%d%c", &size, &deleter) == 0)
 	{
 		
 	}
-	int *massive=(int*)malloc(sizeof(int)*size);
-	for(int i=0;i<size;i++)
+	int *massive = (int*)malloc(sizeof(int) * size);
+	for(int i=0; i<size; i++)
 	{
-		if(scanf("%d%c",massive+i,&deleter)==0)
+		if(scanf("%d%c", massive+i, &deleter) == 0)
 		{
 			
 		}
 	}
-	heapsort(massive,size);
-	printer_mass(massive,size);
+	heapsort(massive, size);
+	print_arr(massive, size);
 	free(massive);
 	return 0;
 }
