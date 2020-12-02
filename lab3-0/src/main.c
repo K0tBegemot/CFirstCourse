6c08963c00d6c53f3b93333b049696dd1447d462
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-12-02 17:29:41 +0700

    Update main.c

--- lab3-0/src/main.c
+++ lab3-0/src/main.c
@@ -2,77 +2,69 @@
 #include <stdlib.h>
 #include <string.h>
 
-void quicksort(int *mass,int first,int end)
+void quicksort(int *mass, int first, int last)
 {
 	
-	if(end-first==0)//end-first==1||
+	if (last - first == 0)
 	{
 		
 	}else
 	{
 	
-		int pivot=mass[(first+end)/2];
-		int i=first,ii=end;
-		while(i<=ii)
+		int pivot = mass[(first + last) / 2];
+		int i = first,ii = last;
+		while (i <= ii)
 		{
-			while(mass[i]<pivot)
+			while (mass[i] < pivot)
 			{
-				i+=1;
+				i += 1;
 			}
-			while(mass[ii]>pivot)
+			while (mass[ii] > pivot)
 			{
-				ii-=1;
+				ii -= 1;
 			}
-			if(i<=ii)
+			if (i <= ii)
 			{
 			int t;
-			t=mass[i];
-			mass[i]=mass[ii];
-			mass[ii]=t;
-			i+=1;
-			ii-=1;
+			t = mass[i];
+			mass[i] = mass[ii];
+			mass[ii] = t;
+			i += 1;
+			ii -= 1;
 			}
 		}
-        /*
-		for(int iii=0;iii<200;iii++)
-		{
-			printf("%d%c",mass[iii],' ');
-		}
-		printf("%c",'\n');
-		*/
 		if (first < ii)
 		{
 			quicksort(mass, first, ii);
 		}
-        if (i < end)
+        if (i < last)
         {
-        	quicksort(mass, i, end);
+        	quicksort(mass, i, last);
 		}
 	}
 }
 
 int main()
 {
-	FILE *ptrfile=fopen("in.txt","r");
-	char delet;
+	FILE *ptrfile = fopen("in.txt", "r");
 	int size;
-	if(fscanf(ptrfile,"%d%c",&size,&delet)==0)
+	if (fscanf(ptrfile, "%d", &size) == 0)
 	{
 		
 	}
-	int *mass=(int*)malloc(sizeof(int) * size);
-	for(int i=0;i<size;i++)
+	int *mass = (int*)malloc(sizeof(int) * size);
+	for (int i=0; i < size; i++)
 	{
-		if(fscanf(ptrfile,"%d%c",&mass[i],&delet)==0)
+		if (fscanf(ptrfile, "%d", &mass[i]) == 0)
 		{
 		    
 		}
 	}
-	quicksort(mass,0,size-1);
-	FILE* ptrfile2=fopen("out.txt","w");
-	for(int i=0;i<size;i++)
+	quicksort(mass, 0, size-1);
+	FILE* ptrfile2 = fopen("out.txt", "w");
+	for (int i=0; i<size; i++)
 	{
-		fprintf(ptrfile2,"%d%s",mass[i]," ");
+		fprintf(ptrfile2, "%d%c", mass[i], ' ');
 	}
 	free(mass);
 	fclose(ptrfile);
