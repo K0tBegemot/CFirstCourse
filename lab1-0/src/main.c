2c308068130736a143038ca3591d6c3d3247d440
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-12-02 16:32:50 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -4,34 +4,19 @@
 
 int main()
 {
-    char a[18],c[97];
+    char a[18], c[97];
     int table[16];
     char table_w[16];
-    FILE* ptrfile=fopen("in.txt","r");
-    if(fgets(a,18,ptrfile)==0)
+    FILE* ptrfile = fopen("in.txt", "r");
+    if(fgets(a, 18, ptrfile) == 0)
     {
     	fclose(ptrfile);
     	return 0;
 	}
 	long long int a_size = strlen(a);
-    //printf("%d%s%d%c", strlen(a)," ", count,'\n');
-    //long long int counter, 
     long long int table_size = 0;
-    /*
-    for(int i=0;i<a_size;i++)
-    {
-    	printf("%c%s",a[i],"-");
-	}
-	printf("%s","()");
-	for(int i=0;i<b_size;i++)
-	{
-		printf("%c%s",b[i],"-");
-	}
-	*/
-	//if((b_size+1)%a_size==0)
     for (int i = 0; i < a_size-2; i++)
     {
-    	//printf("%d%s",a[i]," ");
          long long int counter = 0;
          for (int ii = 0; ii < table_size; ii++)
          {
@@ -49,119 +34,110 @@ int main()
                 table_size += 1;
          }
     }
-    FILE* ptrfile2=fopen("out.txt","w");
-    long long int gerb=0;
-    while(fgets(c,97,ptrfile)!=0)
+    FILE* ptrfile2 = fopen("out.txt", "w");
+    long long int gerb = 0;
+    while(fgets(c, 97, ptrfile) != 0)
     {
-    	int size_c=strlen(c);
-    	if(size_c==0)
+    	int size_c = strlen(c);
+    	if(size_c == 0)
     	{
     		break;
 		}else
 		{
 		    long long int location = a_size - 2;
-    /*
-    for (int i = 0; i < table_size; i++)
-    {
-        printf("%c%d%s", table_w[i], table[i]," ");
-    }
-    */
-    //printf("%d",location);
-    while (location < size_c)
-    {
-    	fprintf(ptrfile2,"%lld%s", location+1+gerb, " ");
-        if (c[location] == a[a_size - 2])
-        {
-            //printf("%lld%s",location," ");
-            long long int location_copy = location - 1,popp=1;
-            for (int i = a_size - 3; i > -1; i--)
+            while (location < size_c)
             {
-            	fprintf(ptrfile2,"%lld%s", location_copy + 1+gerb," ");
-                if (c[location_copy] == a[i])
+    	        fprintf(ptrfile2, "%lld%s", location + 1 + gerb, " ");
+                if (c[location] == a[a_size - 2])
                 {
-                //printf("%c%s%c", b[location_copy], " ", a[i]);
-                    popp += 1;
-                    location_copy -= 1;
-                }
-                else
-                {
-                    int l = 0;
-                    for (int ii = 0; ii < table_size; ii++)
+                    long long int location_copy = location - 1, popp=1;
+                    for (int i = a_size - 3; i > -1; i--)
                     {
-                        if (table_w[ii] == a[a_size - 2])
+            	        fprintf(ptrfile2, "%lld%s", location_copy + 1 + gerb, " ");
+                        if (c[location_copy] == a[i])
                         {
-                            location += table[ii];
-                            if(location>=size_c)
+                            popp += 1;
+                            location_copy -= 1;
+                        }
+                        else
+                        {
+                            int l = 0;
+                            for (int ii = 0; ii < table_size; ii++)
                             {
-                                gerb+=size_c;	
-							}
-                            l = 1;
+                                if (table_w[ii] == a[a_size - 2])
+                                {
+                                    location += table[ii];
+                                    if(location >= size_c)
+                                    {
+                                        gerb += size_c;	
+							        }
+                                    l = 1;
+                                    break;
+                                }
+                            }
+                            if (l != 1)
+                            {
+                                location += a_size-1;
+                                if(location >= size_c)
+                                {
+                        	        gerb += size_c;
+						        }
+                            }
                             break;
                         }
                     }
-                    if (l != 1)
+                    if (popp == a_size-1)
+                    {
+                        int olp = 0;
+                        for (int i = 0; i < table_size; i++)
+                        {
+                            if(table_w[i] == a[a_size - 2])
+                            {
+                    	        olp = 1;
+                                location += table[i];
+                                if(location >= size_c)
+                                {
+                        	        gerb += size_c;
+						        }
+                                break;
+					        }
+                        }
+                        if (olp != 1)
+                        {
+                            location += a_size-1;
+                            if(location >= size_c)
+                            {
+                            	gerb += size_c;
+				        	}
+                        }
+                    }
+                }
+                else
+                {
+                    int q = 0;
+                    for (int i = 0; i < table_size; i++)
+                    {
+                        if (table_w[i] == c[location])
+                        {
+                            location += table[i];
+                            q = 1;
+                            if(location >= size_c)
+                            {
+                    	        gerb += size_c;
+					        }
+                        break;
+                        }   
+                    }
+                    if (q != 1)
                     {
                         location += a_size-1;
-                        if(location>=size_c)
+                        if(location >= size_c)
                         {
-                        	gerb+=size_c;
-						}
+                        	gerb += size_c;
+				     	}
                     }
-                    break;
                 }
             }
-            if (popp == a_size-1)
-            {
-                int olp = 0;
-                for (int i = 0; i < table_size; i++)
-                {
-                    if(table_w[i] == a[a_size - 2])
-                    {
-                    	olp=1;
-                        location += table[i];
-                        if(location>=size_c)
-                        {
-                        	gerb+=size_c;
-						}
-                        break;
-					}
-                }
-                if (olp != 1)
-                {
-                    location += a_size-1;
-                    if(location>=size_c)
-                    {
-                    	gerb+=size_c;
-					}
-                }
-            }
-        }
-        else
-        {
-            int q = 0;
-            for (int i = 0; i < table_size; i++)
-            {
-                if (table_w[i] == c[location])
-                {
-                    location += table[i];
-                    q = 1;
-                    if(location>=size_c)
-                    {
-                    	gerb+=size_c;
-					}
-                    break;
-                }   
-            }
-            if (q != 1)
-            {
-                location += a_size-1;
-                if(location>=size_c)
-                    {
-                    	gerb+=size_c;
-					}
-            }
-        }
-    }
 		}
 	}
 	fclose(ptrfile);
