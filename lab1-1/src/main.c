dc3d4f3c8451cc435d4edf6aa74c8d961a141b8f
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-12-02 16:42:07 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -11,22 +11,22 @@ int main()
 {
     char a[18], b[1000];
     FILE* ptrfile = fopen("in.txt", "r");
-    if(fgets(a,18,ptrfile)==0)
+    if (fgets(a, 18, ptrfile)==0)
     {
     	fclose(ptrfile);
     	return 0;
 	}
 	int a_size = strlen(a)-1;
-	printf("%d%s",hash(a, 0, a_size), " ");
-	while(fgets(b, 1000,ptrfile)!=0)
+	printf("%d%c", hash(a, 0, a_size), ' ');
+	while (fgets(b, 1000, ptrfile) != 0)
 	{
 		int b_size = strlen(b);
-		if(b[b_size-1]=='\n')
+		if (b[b_size-1] == '\n')
 		{
-			Rob_Carp(a, b, a_size, b_size);
+			Rabin_Karp(a, b, a_size, b_size);
 		}else
 		{
-			Rob_Carp(a, b, a_size, b_size);
+			Rabin_Karp(a, b, a_size, b_size);
 			break;
 		}
 	}
@@ -34,7 +34,7 @@ int main()
     return 0;
 }
 
-void Rob_Carp(char* a, char* b, int a_size, int b_size)
+void Rabin_Karp(char* a, char* b, int a_size, int b_size)
 {
     int hashstr = hash(b, 0, a_size);
     int hashsubstr = hash(a, 0, a_size);
@@ -44,8 +44,8 @@ void Rob_Carp(char* a, char* b, int a_size, int b_size)
         {
             for (int ii = i; ii < i + a_size; ii++)
             {
-                printf("%d%s", counter+ii + 1, " ");
-                if(a[ii-i]!=b[ii])
+                printf("%d%c", counter+ii + 1, ' ');
+                if(a[ii-i] != b[ii])
                 {
                 	break;
 				}
@@ -53,36 +53,23 @@ void Rob_Carp(char* a, char* b, int a_size, int b_size)
         }
         hashstr = hash(b, i+1, a_size);
     }
-    counter+=(b_size);
+    counter += (b_size);
 }
 
 int hash(char* aa, int firstel, int a_size)
 {
     int hasher = 0;
-    //printf("%d%s%d%c", firstel, " ", a_size,'\n');
-    //int los=0;
     for (int i = firstel; i < firstel + a_size; i++)
     {
-    	int k=0;
-    	if((int)aa[i]<0)
+    	int k = 0;
+    	if((int)aa[i] < 0)
     	{
-    		k=193+(int)aa[i];
-    	    /*
-    		printf("%d%s",k," ");
-    		los+=1;
-    	    */
+    		k = 193 + (int)aa[i];
 		}else
 		{
-			k=(int)aa[i];
+			k = (int)aa[i];
 		}
-        hasher += ((k % 3) * pow(3, (i-firstel)));
+        hasher += ((k % 3) * pow(3, (i - firstel)));
     }
-    /*
-    if(los>0)
-    {
-    	printf("%c",'\n');
-	}
-	*/
-    //printf("%d%c", hasher, '\n');
     return hasher;
 }
