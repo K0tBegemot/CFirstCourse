580dc62ebd3815aedc5b2f76f6b0fd50863d0541
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2020-12-02 16:09:10 +0700

    Update main.c

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -4,17 +4,24 @@
 
 int main()
 {
-	long long int indx=0,tochka=0,treq=0,part1=0,part2=0,step=1,part1_size=0,p1=0;
-	int a,b;
-	char X[14],j;
-	FILE* ptrfile=fopen("in.txt","r");
-	if(fscanf(ptrfile,"%d",&a)==0)
+	long long int = 0, tochka = 0, treq = 0, part1 = 0, part2 = 0, step = 1, part1_size = 0, p1=0;
+	int a, b;
+	char X[14], j;
+	FILE* ptrfile = fopen("in.txt", "r");
+	if (fscanf(ptrfile, "%d%d%c%13s", &a, &b, &j, X) != 3)
+	{
+	    printf("bad input\n");
+	    fclose ptrfile;
+	    return 0;
+	}
+	/*
+	if (fscanf(ptrfile, "%d", &a) == 0)
 	{
 	    printf("%s","bad input");
 	    fclose(ptrfile);
 		return 0;
 	}
-	if(fscanf(ptrfile,"%d",&b)==0)
+	if (fscanf(ptrfile, "%d", &b) == 0)
 	{
 	    printf("%s","bad input");
 	    fclose(ptrfile);
@@ -32,25 +39,26 @@ int main()
 	    fclose(ptrfile);
 		return 0;
 	}
-	if(a<2||a>16||b<2||b>16)
+	*/
+	if (a < 2 || a > 16 || b < 2 || b > 16)
 	{
-	    printf("%s", "bad input");
+	    printf("bad input\n");
 	    fclose(ptrfile);
 		return 0;
 	}
-	int X_size=strlen(X);
-	for (indx=0; indx <X_size; indx++)
+	int xSize = strlen(X);
+	for (indx=0; indx <xSize; indx++)
 	{
-	    if(X[indx]!='\n')
+	    if(X[indx] != '\n')
 	    {
-	        	    int numeral=-1;
-		if(X[indx]-'.'==0)
+	        int numeral = -1;
+		if(X[indx] == '.')
 		{
 			tochka=1;
 			numeral=0;
 		}else
 		{
-			if(X[indx]-'0'>=0&&X[indx]-'0'<=9)
+			if('0' <= X[indx] && X[indx] <= '9')
 			{
 				numeral = X[indx] - '0';
 			}else
@@ -72,28 +80,28 @@ int main()
 		}
 		if (numeral != -1)
 		{
-		    if(numeral<0||numeral>a-1)
+		    if(numeral < 0 || numeral > a-1)
 		    {
 		        printf("%s", "bad input");
 		        fclose(ptrfile);
 			    return 0;
 		    }
-		    if(tochka==0)
+		    if(tochka == 0)
 		    {
 		        part1 = numeral + part1 * a;
 		    }else
 		    {
-		        if(tochka==1)
+		        if(tochka == 1)
 		        {
 		            p1+=1;
-		            if(indx==0)
+		            if(indx == 0)
 		            {
 		                printf("%s", "bad input");
 		                fclose(ptrfile);
 			            return 0;
 		            }else
 		            {
-		                if(X[indx+1]==0)
+		                if(X[indx+1] == 0)
 		                {
 		                    printf("%s", "bad input");
 		                    fclose(ptrfile);
@@ -104,9 +112,9 @@ int main()
 				    continue;
 		        }else
 		        {
-		            if(tochka==2)
+		            if(tochka == 2)
 		            {
-		                treq+=1;
+		               treq += 1;
 		               part2 = part2 * a + numeral;
 				       step *= a; 
 		            }
@@ -128,8 +136,8 @@ int main()
 	    fclose(ptrfile);
 		return 0;
 	}
-	long long int copy_part1=part1,copy_part2=part2;
-	if(part1==0&&part2==0&&tochka==2)
+	long long int copy_part1=part1, copy_part2=part2;
+	if(part1 == 0 && part2 == 0 && tochka == 2)
 	{
 		printf("%s","bad input");
 		return 0;
@@ -139,7 +147,7 @@ int main()
 		copy_part1 /= b;
 		part1_size += 1;
 	}
-	if(indx>0&&part1_size==0)
+	if(indx > 0 && part1_size == 0)
 	{
 	    part1_size=1;
 	}
@@ -158,9 +166,9 @@ int main()
 		}
 		part1_end[i] = symb;
 	}
-	for(long long int i=0;i<15;i++)
+	for(long long int i=0; i<15; i++)
 	{
-			symbol =(part2*b)/step;
+			symbol = (part2*b)/step;
 	    if (symbol <= 9)
 		{
 			symb = (char)((int)'0' + (int)symbol);
@@ -170,20 +178,20 @@ int main()
 			symb = (char)((int)'a' + (int)symbol - 10);
 		}
 		part2_end[i] = symb;
-		part2=part2*b-symbol*step;
+		part2 = part2*b - symbol*step;
 	}
-	for(long long int i=part1_size-1;i>-1;i--)
+	for(long long int i = part1_size-1; i>-1; i--)
 	{
-	    printf("%c",part1_end[i]);
+	    printf("%c", part1_end[i]);
 	}
-	if(tochka==2&&copy_part2!=0)
+	if(tochka == 2 && copy_part2 != 0)
 	{
-		printf("%c",'.');
-		for(long long int i=0;i<12;i++)
+		printf("%c", '.');
+		for(long long int i=0; i<12; ++)
 		{
-			if(part2_end[i]!='-')
+			if(part2_end[i] != '-')
 			{
-				printf("%c",part2_end[i]);
+				printf("%c", part2_end[i]);
 			}
 		}
 	}
