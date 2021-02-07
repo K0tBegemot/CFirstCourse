56ee446ff4d3e073c75715df1d6d2bd0a6b85520
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2021-02-07 20:59:13 +0700

    Update main.c

--- lab6/src/main.c
+++ lab6/src/main.c
@@ -1,5 +1,171 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+typedef struct AVLTree
+{
+    int value;
+    int height;
+    struct AVLTree *LeftKey;
+    struct AVLTree *RightKey;
+} AVL;
+
+typedef struct TreeHead
+{
+    struct AVLTree *Top;
+} TH;
+
+TH *CreateTree()
+{
+    TH *Head;
+    Head = (TH *)malloc(sizeof(TH));
+    Head->Top = 0;
+    return Head;
+}
+
+int GetHeight(AVL *elem)
+{
+    return (elem) ? elem->height : 0;
+}
+
+void RestorationOfCorrectHeight(AVL *elem)
+{
+    int RH = GetHeight(elem->RightKey);
+    int LH = GetHeight(elem->LeftKey);
+    elem->height = ((RH > LH ? RH : LH) + 1);
+}
+
+int CalculateHeightDifference(AVL *elem)
+{
+    return GetHeight(elem->LeftKey) - GetHeight(elem->RightKey);
+}
+
+void SmallRightSpin(AVL *elem)
+{
+    AVL *OldTop = elem;
+    elem = elem->LeftKey;
+    OldTop->LeftKey = elem->RightKey;
+    elem->RightKey = 0;
+    RestorationOfCorrectHeight(elem->RightKey);
+    RestorationOfCorrectHeight(elem);
+}
+
+void SmallLeftSpin(AVL *elem)
+{
+    AVL *OldTop = elem;
+    elem = elem->RightKey;
+    OldTop->RightKey = elem->LeftKey;
+    elem->LeftKey = 0;
+    RestorationOfCorrectHeight(elem->LeftKey);
+    RestorationOfCorrectHeight(elem);
+}
+
+void BigRightSpin(AVL *elem)
+{
+    SmallLeftSpin(elem->LeftKey);
+    SmallRightSpin(elem);
+    RestorationOfCorrectHeight(elem->RightKey);
+    RestorationOfCorrectHeight(elem->LeftKey);
+    RestorationOfCorrectHeight(elem);
+}
+
+void BigLeftSpin(AVL *elem)
+{
+    SmallRightSpin(elem->RightKey);
+    SmallLeftSpin(elem);
+    RestorationOfCorrectHeight(elem->RightKey);
+    RestorationOfCorrectHeight(elem->LeftKey);
+    RestorationOfCorrectHeight(elem);
+}
+
+void Balance(AVL *elem)
+{
+    int HeightDifference = CalculateHeightDifference(elem);
+    if (HeightDifference == 2)
+    {
+        if (CalculateHeightDifference(elem->LeftKey) >= 0)
+        {
+            SmallRightSpin(elem);
+        }
+        else
+        {
+            BigRightSpin(elem);
+        }
+    }
+    else
+    {
+        if (HeightDifference == -2)
+        {
+            if (CalculateHeightDifference(elem->RightKey) <= 0)
+            {
+                SmallLeftSpin(elem);
+            }
+            else
+            {
+                BigLeftSpin(elem);
+            }
+        }
+    }
+}
+
+AVL *InsertElement(AVL *elem, int a)
+{
+    if (!elem)
+    {
+        AVL *NewLeaf;
+        NewLeaf = (AVL *)malloc(sizeof(AVL));
+        NewLeaf->height = 1;
+        NewLeaf->LeftKey = 0;
+        NewLeaf->RightKey = 0;
+        NewLeaf->value = a;
+        return NewLeaf;
+    }
+    if (a < elem->value)
+    {
+        elem->height += 1;
+        elem->LeftKey = InsertElement(elem->LeftKey, a);
+    }
+    else
+    {
+        if (a > elem->value)
+        {
+            elem->height += 1;
+            elem->RightKey = InsertElement(elem->RightKey, a);
+        }
+    }
+    Balance(elem);
+    return elem;
+}
+
+void clear(AVL *elem)
+{
+    if ((!(elem->RightKey)) && (!(elem->LeftKey)))
+    {
+        free(elem);
+    }
+    else
+    {
+        if (elem->RightKey)
+        {
+            clear(elem->RightKey);
+        }
+        if (elem->LeftKey)
+        {
+            clear(elem->LeftKey);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    TH *NewTree = CreateTree();
+    int tmp=0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tmp);
+        NewTree->Top=InsertElement(NewTree->Top, tmp);
+    }
+    printf("%d", GetHeight(NewTree->Top));
+    return 0;
 }
