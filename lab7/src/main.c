81decb26792758b6187dbcc814f90129a7c2ba16
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2021-03-22 21:16:51 +0700

    Update main.c

--- lab7/src/main.c
+++ lab7/src/main.c
@@ -1,227 +1,256 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Stack
-{
-	int TreeNode;
-	struct Stack *Next;
-} st;
+//struct BitFlag
 
-typedef struct TopOfStack
+struct BitSet
 {
-	int length;
-	st *Top;
-} tost;
+    char *bitset;
+    int length;
+    int width;
+    int type; // 0 - symmetrical; 1 - unsymmetrical;
+};
 
-tost *CreateStack(int X)
+struct BitSet *CreateBitSet(int length, int width, int type)
 {
-	tost *Array = (tost *)malloc(sizeof(tost) * X);
-	for (int i = 0; i < X; i++)
-	{
-		(Array + i)->length = 0;
-		(Array + i)->Top = 0;
-	}
-	return Array;
+    if (length == 0 || width == 0)
+    {
+        printf("Invalid length or value field. Bitset not created");
+        return 0;
+    }
+    struct BitSet *created = (struct BitSet *)malloc(sizeof(struct BitSet));
+    created->bitset = 0;
+    int size = length * width;
+    int summ;
+    switch (type)
+    {
+    case 0:
+        created->bitset = (char *)calloc((size / 8 + (size % 8 ? 1 : 0)), sizeof(char));
+        break;
+    case 1:
+        summ = ((width - 1) * width / 2);
+        created->bitset = (char *)calloc(((size - summ) / 8 + ((size - summ) % 8 ? 1 : 0)), sizeof(char));
+        break;
+    default:
+        printf("Invalid type field. Bitset not created");
+        free(created);
+        return 0;
+    }
+    created->length = length;
+    created->width = width;
+    created->type = type;
+    return created;
 }
 
-int Empty(tost *Head)
+int WriteBit(struct BitSet *created, int x, int y, int bit)
 {
-	if (Head->Top)
-	{
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
+    switch (created->type)
+    {
+    case 0:
+        if (x + 1 > created->width || y + 1 > created->length)
+        {
+            printf("Invalid coordinate fields. Bit not writed");
+            return 0;
+        }
+        int shift = (created->length) * (x) + y;
+        char *tVar = (created->bitset) + (shift) / 8;
+        if ((*tVar & (1 << (8 - (shift % 8) - 1))) != 0)
+        {
+            if (bit == 0)
+            {
+                *tVar = (*tVar ^ (1 << (8 - (shift % 8) - 1)));
+            }
+        }
+        else
+        {
+            if (bit == 1)
+            {
+                *tVar = (*tVar ^ (1 << (8 - (shift % 8) - 1)));
+            }
+        }
+        return 1;
+        break;
+    case 1:
+
+        break;
+    default:
+        printf("Invalid type field. Bad BitSet. Bit not writed");
+        return 0;
+    }
 }
 
-int PopFromStack(tost *Head)
+int ReadBit(struct BitSet *created, int x, int y)
 {
-	if (Empty(Head))
-	{
-		return -1;
-	}
-	else
-	{
-		int a = Head->Top->TreeNode;
-		st *OldTop = Head->Top;
-		Head->Top = Head->Top->Next;
-		free(OldTop);
-		Head->length -= 1;
-		return a;
-	}
+    switch (created->type)
+    {
+    case 0:
+        if (x + 1 > created->width || y + 1 > created->length)
+        {
+            printf("Invalid coordinate fields. Bit not readed");
+            return 0;
+        }
+        int shift = (created->length) * (x) + y;
+        char *tVar = (created->bitset) + (shift) / 8;
+        if ((*tVar & (1 << (8 - (shift % 8) - 1))) != 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        break;
+    case 1:
+
+        break;
+    default:
+        printf("Invalid type field. Bad BitSet. Bit not readed");
+        return 0;
+    }
 }
 
-void DeleteStack(st* Top)
+int TopologicSort(struct BitSet *a, int *colorArray, int *finishStack, int *finishStackInd, int *numberOfBlackTops, int whiteTop, int n)
 {
-	if(Top)
-	{
-		if(Top->Next)
-	    {
-	    	DeleteStack(Top->Next);
-    	}
-		free(Top);
-	}
-}
-
-void DeleteStacks(tost *Head, int X)
-{
-	for(int i=0;i<X;i++)
-	{
-		DeleteStack((Head+i)->Top);
-	}
-	free(Head);
-}
-
-void PushToStack(tost *Head, int element)
-{
-	st *NewElement = (st *)malloc(sizeof(st));
-	NewElement->TreeNode = element;
-	NewElement->Next = Head->Top;
-	Head->Top = NewElement;
-	Head->length += 1;
-}
-
-int TopologicSort(int *ColorArray, tost *Array, int *NumberOfBlackTops, int WhiteTop, tost *FinishStack)
-{
-	if (ColorArray[WhiteTop] == 0)
-	{
-		ColorArray[WhiteTop] = 1;
-	}
-	else
-	{
-		if (ColorArray[WhiteTop] == 1)
-		{
-			return 1;
-		}
-		else
-		{
-			if (ColorArray[WhiteTop] == 2)
-			{
-				return 0;
-			}
-		}
-	}
-	st *Element = (Array + WhiteTop)->Top;
-	for (int i = 0; i < (Array + WhiteTop)->length; i++)
-	{
-		if (Element == 0)
-		{
-			break;
-		}
-		if (TopologicSort(ColorArray, Array, NumberOfBlackTops, (Element->TreeNode) - 1, FinishStack))
-		{
-			return 1;
-		}
-		Element = Element->Next;
-	}
-	ColorArray[WhiteTop] = 2;
-	*NumberOfBlackTops += 1;
-	PushToStack(FinishStack, WhiteTop + 1);
-	return 0;
+    if (colorArray[whiteTop] == 0)
+    {
+        colorArray[whiteTop] = 1;
+    }
+    else
+    {
+        if (colorArray[whiteTop] == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            if (colorArray[whiteTop] == 2)
+            {
+                return 0;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if ((ReadBit(a, whiteTop, i) == 1) && (i != whiteTop))
+        {
+            if (TopologicSort(a, colorArray, finishStack, finishStackInd, numberOfBlackTops, i, n))
+            {
+                return 1;
+            }
+        }
+    }
+    colorArray[whiteTop] = 2;
+    *numberOfBlackTops += 1;
+    finishStack[*finishStackInd] = whiteTop + 1;
+    *finishStackInd += 1;
+    return 0;
 }
 
 int main()
 {
-	int n, m;
-	if(scanf("%d", &n)==0)
-	{
-		printf("bad number of vertices");
-		return 0;
-	}
-	if(scanf("%d", &m)==0)
-	{
-		printf("bad number of lines");
-		return 0;
-	}
-	tost *Array = CreateStack(n);
-	int *ColorArray = (int *)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; i++)
-	{
-		ColorArray[i] = 0;
-	}
-	int f, s;
-	int exceptions = 0, counter=0;
-	for (int i = 0; i < m; i++)
-	{
-		if(scanf("%d%d", &f, &s)<2)
-		{
-			break;
+    int n, m;
+    if (scanf("%d", &n) == 0)
+    {
+        printf("bad number of vertices");
+        return 0;
+    }
+    struct BitSet *a = CreateBitSet(n, n, 0);
+    if (scanf("%d", &m) == 0)
+    {
+        printf("bad number of lines");
+        return 0;
+    }
+    int *colorArray = (int *)calloc(n, sizeof(int));
+    int f, s;
+    int exceptions = 0, counter = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        if (scanf("%d%d", &f, &s) < 2)
+        {
+            break;
+        }
+        counter += 1;
+        if (f == s)
+        {
+            exceptions = 1;
+            break;
+        }
+        else
+        {
+            if ((f < 1 || f > n) || (s < 1 || s > n))
+            {
+                exceptions = 2;
+            }
+        }
+        WriteBit(a, f - 1, s - 1, 1);
+    }
+    if (counter < m)
+    {
+        exceptions = 1;
+    }
+    if (n < 0 || n > 2000)
+    {
+        printf("bad number of vertices");
+        return 0;
+    }
+    else
+    {
+        if (m < 0 || m > (n * (n - 1)) / 2)
+        {
+            printf("bad number of edges");
+            return 0;
+        }
+        else
+        {
+            if (exceptions == 1)
+            {
+                printf("bad number of lines");
+                return 0;
+            }
+            else
+            {
+                if (exceptions == 2)
+                {
+                    printf("bad vertex");
+                    return 0;
+                }
+            }
+        }
+    }
+    /*
+    for(int i=0;i<n;i++)
+    {
+    	for(int ii=0;ii<n;ii++)
+    	{
+    		printf("%d ", ReadBit(a,i,ii));
 		}
-		counter+=1;
-		if (f == s)
-		{
-			exceptions = 1;
-			break;
-		}
-		else
-		{
-			if ((f < 1 || f > n) || (s < 1 || s > n))
-			{
-				exceptions = 2;
-			}
-		}
-		PushToStack(&Array[f - 1], s);
+		printf("\n");
 	}
-	if(counter<m)
-	{
-		exceptions = 1;
-	}
-	if (n < 0 || n > 2000)
-	{
-		printf("bad number of vertices");
-		return 0;
-	}
-	else
-	{
-		if (m < 0 || m > (n * (n - 1)) / 2)
-		{
-			printf("bad number of edges");
-			return 0;
-		}
-		else
-		{
-			if (exceptions == 1)
-			{
-				printf("bad number of lines");
-				return 0;
-			}
-			else
-			{
-				if (exceptions == 2)
-				{
-					printf("bad vertex");
-					return 0;
-				}
-			}
-		}
-	}
-	int NumberOfWhiteTops = n;
-	tost *FinishStack = CreateStack(1);
-	while (NumberOfWhiteTops > 0)
-	{
-		int NumberOfBlackTops = 0, WhiteTop=0;
-		for (int i = 0; i < n; i++)
-		{
-			if (ColorArray[i] == 0)
-			{
-				WhiteTop = i;
-				break;
-			}
-		}
-		if (TopologicSort(ColorArray, Array, &NumberOfBlackTops, WhiteTop, FinishStack))
-		{
-			printf("impossible to sort");
-			return 0;
-		}
-		NumberOfWhiteTops -= NumberOfBlackTops;
-	}
-	while (!Empty(FinishStack))
-	{
-		printf("%d ", PopFromStack(FinishStack));
-	}
-	DeleteStacks(FinishStack,1);
-	DeleteStacks(Array,n);
-	free(ColorArray);
+	*/
+    int numberOfWhiteTops = n;
+    int *finishStack = malloc(sizeof(int) * n);
+    int finishStackInd = 0;
+    while (numberOfWhiteTops > 0)
+    {
+        int numberOfBlackTops = 0, whiteTop = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (colorArray[i] == 0)
+            {
+                whiteTop = i;
+                break;
+            }
+        }
+        if (TopologicSort(a, colorArray, finishStack, &finishStackInd, &numberOfBlackTops, whiteTop, n))
+        {
+            printf("impossible to sort");
+            return 0;
+        }
+        numberOfWhiteTops -= numberOfBlackTops;
+    }
+    for (int i = n - 1; i > -1; i--)
+    {
+        printf("%d", finishStack[i]);
+    }
 }
