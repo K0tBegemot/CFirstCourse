65c1e582a4d7f64437ff8535ed5b2eb5cf996640
Бакелев, Константин Валерьевич <k.bakelev@g.nsu.ru>
2021-03-24 21:25:41 +0700

    Update main.c

--- lab8-0/src/main.c
+++ lab8-0/src/main.c
@@ -97,6 +97,7 @@ void MergeConnectedComponent(Graph *a, int set1, int set2)
     for(int i=0;i<a->vertices;i++)
     {
     	int var= FindConnectedComponent(a,i);
+    	var=0;
 	}
     if (((a->cc->rank)[set1] == (a->cc->rank)[set2])&&((a->cc->rank)[set2]==0))
     {
